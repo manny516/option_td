dc1c8d90f3e3394eea70d513d9da56ef702b2f48
ww2kvn <ww2kvn@ww2kvns-MacBook-Pro.local>
2019-03-20 12:04:48 -0400

    Added Header Compononet to App compononet

--- trade_options/src/comp/Header.js
+++ trade_options/src/comp/Header.js
@@ -1,1 +1,19 @@
-console.log("Header");
+import React, {Component} from 'react';
+
+class Header extends Component{
+
+	render(){
+
+		return(
+			<h1> Options Trade App</h1>
+		);
+
+	}
+
+
+}
+
+export default Header; 
+
+
+
