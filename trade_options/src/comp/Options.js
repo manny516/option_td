55809e8fbedb4115d3664f4c516b1a99fbda723c
ww2kvn <ww2kvn@ww2kvns-MacBook-Pro.local>
2019-03-27 15:38:25 -0400

    add TD Ameritrade fetch API promise to grab data stock Data from API

--- trade_options/src/comp/Options.js
+++ trade_options/src/comp/Options.js
@@ -4,11 +4,36 @@ class Options extends Component{
 
 	constructor(props){
 		super(props);
+		this.fetchData = this.fetchData.bind(this);
+	}
+
+	fetchData(symbol){
+		// The API path for the Options Trade data 
+
+		const url = `https://api.tdameritrade.com/v1/marketdata/chains?apikey=OPTIONS50&symbol=${symbol}`;
+		const options = {
+
+			methods : 'GET',
+			headers: {
+				"Authorization": "Bearer <inter Acccess token here> "
+			}
+		}
+
+		fetch(url, options)
+		.then(results => results.json())
+		.then(results  => console.log(results));
+	}
+
+	componentDidMount(){
+		this.fetchData("AAPL");
+		this.fetchData("WDAY");
+		this.fetchData("VTWO");
+		this.fetchData("WSBC");
+
 	}
 
 	render(){
 		return();
-
 	}
 
 
