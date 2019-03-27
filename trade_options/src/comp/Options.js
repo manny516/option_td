d08d912b45aba69155c9c3067238ec7b73736602
ww2kvn <ww2kvn@ww2kvns-MacBook-Pro.local>
2019-03-27 15:26:28 -0400

    create Options component to display options Data

---
+++ trade_options/src/comp/Options.js
@@ -0,0 +1,17 @@
+import React, {Components} from 'react'
+
+class Options extends Component{
+
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		return();
+
+	}
+
+
+}
+
+export default Options;
