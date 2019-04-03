c8547adf9e6e33975f14ccf0bc5f366085a05ab5
manny516 <mwalthrust@gmail.com>
2019-04-03 15:27:16 -0400

    added State to the FilterSearch Component. Track input field key stroke and successfully eval input stroke.

--- trade_options/src/comp/FilterSearch.js
+++ trade_options/src/comp/FilterSearch.js
@@ -8,9 +8,34 @@ class FilterSearch extends Component {
 	constructor(props){
 		super(props)
 
+		this.state = {value : "Search Stock"}
 		this.nasData = TickerData[0]["NASDAQ"];
 		this.amexData = TickerData[1]["AMEX"];
 		this.nysData = TickerData[2]["NYSCE"];
+		this.handleChange = this.handleChange.bind(this);
+		this.outPut = this.outPut.bind(this);
+	}
+
+
+
+	
+
+	handleChange(e){
+		let state = this.setState( {value : e.target.value} );
+		return state;
+	}
+
+	outPut(){	
+
+		console.log(this.state.value);
+
+		if(this.state.value === "AAPL"){
+			console.log("It's a Match");
+		}else{
+			console.log("No MAtch");	
+		}
+
+
 	}
 
 	render(){
@@ -18,9 +43,8 @@ class FilterSearch extends Component {
 		return(
 
 			<React.Fragment>
-
 				<div className="ticker-filter"> 
-					<input type="text" name="filter-search"/>
+					<input type="text" placeholder={this.state.value} onChange={this.handleChange} onKeyUp={this.outPut} name="filter-search"/>
 				</div>
 
 
@@ -29,7 +53,7 @@ class FilterSearch extends Component {
 					<Ticker exchangeName="NYSCE" stockTicker={this.nysData} />
 					<Ticker exchangeName="AMEX" stockTicker={this.amexData} />
 				</div>
-				
+
 			</React.Fragment>
 
 		)
